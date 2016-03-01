ab56b3d920bb785a5615335082d0c70d47476a0f
raul <racobos@ucm.es>
2016-03-02 00:01:57 +0100

    Created destroy collider for arkanoid ball

---
+++ PROJECT/Assets/Projects/Arkanoid/Scripts/BallIA.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallIA : MonoBehaviour {
+
+	public int forceX;
+	public int forceY;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(forceX,forceY));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
