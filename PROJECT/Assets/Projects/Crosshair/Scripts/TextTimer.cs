5aef8ff116b937434a9d2ea53707f907cfa31a0b
raul <racobos@ucm.es>
2016-03-14 21:59:25 +0100

    Added more scripts

---
+++ PROJECT/Assets/Projects/Crosshair/Scripts/TextTimer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextTimer : MonoBehaviour
+{
+	public float lifeTime = 2f;
+	void Update ()
+	{
+		if (this.GetComponent<Text> ().isActiveAndEnabled)
+			Destroy (gameObject, lifeTime);
+	}
+
+}
