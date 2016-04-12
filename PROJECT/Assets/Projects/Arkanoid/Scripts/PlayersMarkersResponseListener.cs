f003f69a6e9d25c54c9b741c9d40bfab4d1140fc
raul <racobos@ucm.es>
2016-04-13 00:45:47 +0200

    Implemeneted api rest communication

---
+++ PROJECT/Assets/Projects/Arkanoid/Scripts/PlayersMarkersResponseListener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayersMarkersResponseListener : MonoBehaviour, ClientApi.ResponseWWWListener {
+
+	public Text board;
+
+	public void success (WWW www)
+	{
+		board.text = www.text;	
+	}
+
+	public void error (WWW www)
+	{
+		Debug.LogError (www.error);
+		board.text = "Se produjo un error y no se tramitó la petición correctamente";
+	}
+}
