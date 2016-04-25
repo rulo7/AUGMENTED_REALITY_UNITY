a109890ae30fe92928106f339b7275d00030bfe4
raul <racobos@ucm.es>
2016-04-26 00:14:37 +0200

    Completed starting scene

---
+++ PROJECT/Assets/Projects/Global/Scripts/GlobalGameManajer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GlobalGameManajer : MonoBehaviour {
+
+	private static GlobalGameManajer instance;
+
+	public static GlobalGameManajer geInstance(){
+		if (instance== null){
+			GameObject go = new GameObject();
+			instance = go.AddComponent<GlobalGameManajer>();
+		}
+		return instance;
+	}
+
+	public void loadArkanoid(){
+		SceneManager.LoadScene ("ArkanoidScene");
+		unloadActiveScene();
+	}
+
+	public void loadCrosshair(){
+		SceneManager.LoadScene ("crosshair");
+		unloadActiveScene();
+	}
+
+	public void loadWaterPipes(){
+		SceneManager.LoadScene ("menu");
+		unloadActiveScene();
+	}
+
+	private void unloadActiveScene(){
+		SceneManager.UnloadScene (SceneManager.GetActiveScene().name);
+	}
+}
---
+++ PROJECT/Assets/Projects/Start/Scripts/OnClickListener.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickListener : MonoBehaviour {
+
+	public void OnClick () {
+		GlobalGameManajer.geInstance ().loadArkanoid ();
+	}
+}
