62272ced32ee42fd26fd3f71591d1bdc3e3f262d
alvardsoler <alsoler@ucm.es>
2016-04-29 14:38:23 +0200

    Añadidos sonidos de láser y explosión. Corregido un poco el punto de mira láser.

--- PROJECT/Assets/Projects/Crosshair/Scripts/Shooter.cs
+++ PROJECT/Assets/Projects/Crosshair/Scripts/Shooter.cs
@@ -6,12 +6,33 @@ public class Shooter : MonoBehaviour
 
 	public    GameObject shot;
 	private LineRenderer line;
+	private 		AudioSource audioS;
+	// web http://soundbible.com/472-Laser-Blasts.html
+	// explosion http://soundbible.com/1234-Bomb.html
 	void Start ()
 	{
 		line = GetComponent<LineRenderer> ();
 		line.enabled = false;
+		audioS = GetComponent<AudioSource> ();
 	}
 	// Update is called once per frame
+	private void Shoot ()
+	{
+		RaycastHit hit;
+		//Camera c = GameManager.instance.getCamera ();
+		Ray ray = Camera.main.ScreenPointToRay (new Vector3 ((Screen.width / 2), (Screen.height / 2), 0));
+		audioS.Play ();
+		if (Physics.Raycast (ray, out hit)) {
+			//hit.collider.SendMessage ("OnCollisionEnter", this);			
+			Invader i = hit.collider.gameObject.GetComponentInParent<Invader> ();
+			if (i != null) {
+				i.Hit ();
+			}
+			//hit.collider.BroadcastMessage ("OnCollisionEnter", this);
+			Debug.Log (hit.collider.gameObject.name.ToString ());
+
+		}
+	}
 	void Update ()
 	{
 		// probar a trampear con una buena foto y volver a imageTarget
@@ -20,14 +41,7 @@ public class Shooter : MonoBehaviour
 			if (!line.enabled)
 				line.enabled = true;
 			if (Input.GetMouseButtonDown (0)) {
-				RaycastHit hit;
-				//Camera c = GameManager.instance.getCamera ();
-				Ray ray = Camera.main.ScreenPointToRay (new Vector3 ((Screen.width / 2), (Screen.height / 2), 0));
-				if (Physics.Raycast (ray, out hit)) {
-					hit.collider.SendMessage ("OnCollisionEnter", this);
-
-					Debug.Log (hit.collider.gameObject.name.ToString ());
-				}
+				Shoot ();
 				/*  GameObject projectile = Instantiate(shot, transform.position + transform.forward * 2, transform.rotation) as GameObject;
                 Rigidbody rb = projectile.GetComponent<Rigidbody>();
                 rb.AddForce(transform.forward * 2000);*/
